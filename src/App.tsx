2ec253049ee648ba1a3acc10fd3b2aa47825b1cc
DkrabbitLuke <luketae0220@dkrabbit.com>
2025-12-10 21:28:28 -0800

    feat: build back bone

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-import React from "react";
 import "./App.css";
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
 import About from "./components/About";
-import Skills from "./components/Skills";
-import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
 import ScrollToTop from "./components/ScrollToTop";
+import Skills from "./components/Skills";
 
 function App() {
   return (
