7671b99f70775556d25f07ce2126695a161c12eb
DkrabbitLuke <luketae0220@dkrabbit.com>
2026-05-10 18:04:34 -0700

    feat: add new technology icons and entries to skills section

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -2,26 +2,38 @@ import React from "react";
 import { IconType } from "react-icons";
 import {
   SiAngular,
+  SiExpress,
+  SiFirebase,
+  SiFlydotio,
   SiGit,
   SiJavascript,
+  SiMantine,
   SiMongodb,
   SiNestjs,
+  SiNextdotjs,
   SiNodedotjs,
   SiReact,
   SiSwagger,
   SiTypescript,
+  SiVercel,
 } from "react-icons/si";
 
 const skills: Array<{ name: string; icon: IconType; color: string }> = [
+  { name: "Next.js", icon: SiNextdotjs, color: "#000000" },
   { name: "Angular", icon: SiAngular, color: "#DD0031" },
   { name: "React", icon: SiReact, color: "#61DAFB" },
   { name: "JavaScript", icon: SiJavascript, color: "#F7DF1E" },
   { name: "TypeScript", icon: SiTypescript, color: "#3178C6" },
   { name: "NestJS", icon: SiNestjs, color: "#E0234E" },
+  { name: "Express", icon: SiExpress, color: "#000000" },
   { name: "MongoDB", icon: SiMongodb, color: "#47A248" },
+  { name: "Firebase", icon: SiFirebase, color: "#FFCA28" },
   { name: "Node.js", icon: SiNodedotjs, color: "#339933" },
   { name: "RESTful APIs", icon: SiSwagger, color: "#85EA2D" },
+  { name: "Mantine UI", icon: SiMantine, color: "#339AF0" },
   { name: "Git", icon: SiGit, color: "#F05032" },
+  { name: "Fly.io", icon: SiFlydotio, color: "#24185B" },
+  { name: "Vercel", icon: SiVercel, color: "#000000" },
 ];
 
 const Skills = () => {
